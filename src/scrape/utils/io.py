26f24041fc61ab2e162b16fab99651de86da52bf
ostosh <ostoshg@gmail.com>
2016-02-13 14:08:00 -0800

    add io reader as seperate util

---
+++ src/scrape/utils/io.py
@@ -0,0 +1,18 @@
+import io
+
+
+class BufferedReader:
+    def __init__(self):
+        self.sio = io.BytesIO()
+        self.cur_size = 0
+
+    def buffer_data(self, data):
+        self.cur_size += len(data)
+        self.sio.write(data)
+
+    def save_file(self, callback):
+        self.sio.seek(0)
+        callback(self.sio)
+
+    def get_buffered_size(self):
+        return self.cur_size

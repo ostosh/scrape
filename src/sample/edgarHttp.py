3ea878f37c99fa6b35f957e769092a75a7dcc1b7
Geoffrey <ostoshg@gmail.com>
2016-02-15 09:39:20 -0800

    update http edgar example with full report index

--- src/sample/edgarHttp.py
+++ src/sample/edgarHttp.py
@@ -1,11 +1,16 @@
+import datetime
+
 from scrape.crawl.http import Http
 from scrape.handlers.text import Text
 
 opts = {}
 opts['url'] = 'http://sec.gov'
-opts['paths'] = ['/Archives/edgar/full-index/1993/QTR1/master.gz',
-                 '/Arhicves/edgar/full-index/1994/QTR1/master.gz',
-                 '/Arhicves/edgar/full-index/1995/QTR1/master.gz']
+
+opts['paths'] = [
+        '/Archives/edgar/full-index/'+str(year)+'/QTR'+str(qtr)+'/master.gz'
+        for year in range(1993, datetime.date.today().year)
+        for qtr in range(1,4+1)
+    ]
 opts['filehandler'] = Text.to_txt
 trans = Http(opts)
 trans.run()

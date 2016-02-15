e39c1d0f46db7eabd3784fa79eb7c2e38e998a80
Geoffrey <ostoshg@gmail.com>
2016-02-15 09:35:13 -0800

    update edgar ftp example for full report index parse

--- src/sample/edgarFtp.py
+++ src/sample/edgarFtp.py
@@ -1,11 +1,41 @@
+import io
+import gzip
+import datetime
+
 from scrape.crawl.ftp import Ftp
 from scrape.handlers.gzip import Gzip
 
+class CustomHandler:
+    @staticmethod
+    def to_stdout(sio):
+        reader = io.BufferedReader(gzip.GzipFile(fileobj=sio))
+        #detect = chardet.detect(reader.peek())
+        with io.TextIOWrapper(reader, encoding="ISO-8859-1") as file:
+            for line in file:
+                if not CustomHandler.is_report_line(line):
+                    continue
+                parsedLine = CustomHandler.parse_line(line)
+                print(parsedLine, end='')
+                print()
+            file.close()
+
+    @staticmethod
+    def is_report_line(line):
+        return line.__contains__("|edgar/data")
+
+    @staticmethod
+    def parse_line(line):
+        split = line.split('|')
+        parsed = {}
+        parsed['cik'] = split[0]
+        parsed['reportType'] = split[2]
+        parsed['fileDate'] = split[3]
+        parsed['path'] = split[4].strip('\n')
+        return parsed
+
 opts = {}
 opts['url'] = 'ftp.sec.gov'
-opts['paths'] = ['/edgar/full-index/1993/QTR1/master.gz',
-                 '/edgar/full-index/1994/QTR1/master.gz',
-                 '/edgar/full-index/1995/QTR1/master.gz']
-opts['filehandler'] = Gzip.to_txt
+opts['paths'] = ['/edgar/full-index/'+str(year)+'/QTR'+str(qtr)+'/master.gz' for year in range(1993, datetime.date.today().year) for qtr in range(1,4+1)]
+opts['filehandler'] = CustomHandler.to_stdout
 trans = Ftp(opts)
 trans.run()

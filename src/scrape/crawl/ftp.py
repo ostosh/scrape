68ca0c7bafd26bcdf8447c366a08112f86f21e72
ostosh <ostoshg@gmail.com>
2016-02-18 23:30:36 -0800

    add improved error handling to ftp crawler

--- src/scrape/crawl/ftp.py
+++ src/scrape/crawl/ftp.py
@@ -1,15 +1,11 @@
-from ftplib import FTP
+from ftplib import FTP, error_reply, error_temp, error_proto
 
 import time 
 import types
 
 from ..handlers.gzip import Gzip
 from ..utils.io import BufferedReader
-from ..utils.time import TimeUtils
-
-
-class FtpError(Exception):
-    pass
+from ..utils.log import LogUtils
 
 
 class IllegalArgumentException(Exception):
@@ -22,12 +18,16 @@ class Ftp:
             raise IllegalArgumentException('Error: invalid props object given.')
         if 'url' not in props or type(props['url']) is not str:
             raise IllegalArgumentException('Error: invalid domain arg of props.')
-        if 'paths' not in props or type(props['paths']) is not list: #TODO add type check for list elems
+        if 'paths' not in props or type(props['paths']) is not list:
             raise IllegalArgumentException('Error: invalid paths arg of props')
+        if 'maxAttempts' not in props or type(props['timeout']) is not int:
+            props['maxAttempts'] = 5
         if 'timeout' not in props or type(props['timeout']) is not int:
             props['timeout'] = 99999999
-        if 'filehandler' not in props or type(props['filehandler']) is not types.FunctionType:
-            props['filehandler'] = Gzip.to_stdout
+        if 'delay' not in props or type(props['timeout']) is not float:
+            props['delay'] = 0.005
+        if 'handler' not in props or type(props['handler']) is not types.FunctionType:
+            props['handler'] = Gzip.to_stdout
         self.props = props
         
     def __init_conn__(self): # TODO add validation for bad path
@@ -47,29 +47,34 @@ class Ftp:
 
     def run(self):
         for path in self.props['paths']:
+            attempts = 0
             while True:
-                try:
-                    self.get_report(path)
-                    time.sleep(0.1)
+                attempts += 1
+                if attempts > self.props['maxAttempts']:
+                    LogUtils.log_error('perm HTTP failure max attempts exceeded while retrieving {0}'.format(path))
                     break
-                except Exception as e: # TODO add validation for bad path
-                    print('Error: FTP failure while downloading. Reseting Connection: {0}. '.format(e))
+                try:
+                    self.retrieve(path)
+                    time.sleep(self.props['delay'])
+                    break
+                except (error_reply, error_temp, error_proto) as e:
+                    LogUtils.log_error('temp FTP failure while retrieving {0}. Resetting {1}'.format(path, e))
                     self.__reset_conn__()
                     continue
+                except Exception as e:
+                    LogUtils.log_error('perm FTP failure while retrieving {0}. Skipping {1}'.format(path,  e))
+                    break
 
-    def get_report(self, uri):
+    def retrieve(self, uri):
         reader = BufferedReader()
         self.ftp.sendcmd('TYPE i')
         report_size = self.ftp.size(uri)
         self.ftp.sendcmd('TYPE a')
-        print('[{0}] GETTING: {1} '.format(TimeUtils.unix_time_mills(), uri))
+        LogUtils.log_info('ftp retrieving {0}'.format(uri))
         
         while reader.get_buffered_size() < report_size:
-                try:
-                    self.ftp.retrbinary('RETR ' + uri, callback=reader.buffer_data)
-                except Exception as e:
-                    raise FtpError(e, uri)
+            self.ftp.retrbinary('RETR ' + uri, callback=reader.buffer_data)
         reader.save_file(self.dump_report)
 
     def dump_report(self, sio):
-        self.props['filehandler'](sio)
+        self.props['handler'](sio)

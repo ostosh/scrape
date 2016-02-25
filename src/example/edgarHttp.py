dbb184e5299402fa3d662451ba99da522eaa43d2
ostosh <ostoshg@gmail.com>
2016-02-24 22:49:50 -0800

    update http example

--- src/example/edgarHttp.py
+++ src/example/edgarHttp.py
@@ -9,8 +9,7 @@ opts['url'] = 'http://sec.gov'
 opts['paths'] = [
         '/Archives/edgar/full-index/'+str(year)+'/QTR'+str(qtr)+'/master.gz'
         for year in range(1993, datetime.date.today().year)
-        for qtr in range(1,4+1)
+        for qtr in range(1, 4+1)
     ]
 opts['handler'] = Gzip.to_stdout
 Http(opts).run()
-

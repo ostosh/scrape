64de7f653e0de24356677791952f9d0fb146422f
ostosh <ostoshg@gmail.com>
2016-02-25 22:29:51 -0800

    add unit test for text handler

---
+++ src/tests/handlers.text.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import unittest
+import io
+import glob
+
+from scrape.handlers.text import Text
+
+
+class TestTextHandlers(unittest.TestCase):
+    def test_to_stdout(self):
+        test_file = open('test.txt', 'w+')
+        sys.stdout = test_file
+        sio = io.BytesIO()
+        sio.write(b"TEST")
+        sio.seek(0)
+        Text.to_stdout(sio)
+        test_file.seek(0)
+        line = test_file.read()
+        test_file.close()
+        os.remove('test.txt')
+        self.assertEqual('TEST', line)
+
+    def test_to_text(self):
+        sio = io.BytesIO()
+        sio.write(b"TEST")
+        sio.seek(0)
+        Text.to_txt(sio)
+        test_file_name = glob.glob("*.txt")[0]
+        test_file = open(test_file_name, 'r')
+        line = test_file.read()
+        test_file.close()
+        os.remove(test_file_name)
+        self.assertEqual('TEST', line)
+
